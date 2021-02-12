79fe0103ea55e2c1d4b35bd75543f966acd1bb13
Gaurav <Gaurav@host.docker.internal>
2021-02-12 12:22:28 +0530

    Initial Stub

---
+++ calculator/src/main/java/com/interview/calculator/StringCalculator.java
@@ -0,0 +1,25 @@
+package com.interview.calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringCalculator 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int Add(String numbers) {
+    	
+    	return 0;
+    	
+    	
+    }
+}
---
+++ calculator/src/test/java/com/interview/calculator/StringCalculatorTest.java
@@ -0,0 +1,20 @@
+package com.interview.calculator;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class StringCalculatorTest 
+{
+    
+    @Test
+    public void addTwoNumbersTest()
+    {
+    	
+        
+    }
+}
