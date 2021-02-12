65161fb61c54badc3e0daa784dfdd61812208954
Gaurav <Gaurav@host.docker.internal>
2021-02-12 12:58:24 +0530

    Refactored : Added BeforeEach and assertAll

--- calculator/src/test/java/com/interview/calculator/StringCalculatorTest.java
+++ calculator/src/test/java/com/interview/calculator/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package com.interview.calculator;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,26 +11,35 @@ import org.junit.jupiter.api.Test;
  */
 public class StringCalculatorTest 
 {
+	StringCalculator strCalc;
+	
+	@BeforeEach
+	public void createObject() {
+		strCalc = new StringCalculator();
+	}
     
     @Test
     public void addWhenEmptyTest()
     {
-    	StringCalculator strCalc = new StringCalculator();
-    	assertEquals(0, strCalc.Add(""));
-    	assertEquals(0, strCalc.Add("  "));
+    	assertAll(
+    			() -> assertEquals(0, strCalc.Add("")),
+    			() -> assertEquals(0, strCalc.Add("  "))
+    			);
     }
     
     @Test
     public void addForSingleNumberTest() {
-    	StringCalculator strCalc = new StringCalculator();
     	assertEquals(7, strCalc.Add("7"));
     	
     }
     
     @Test
     public void addForTwoNumberTest() {
-    	StringCalculator strCalc = new StringCalculator();
     	assertEquals(23, strCalc.Add("12,11"));
+    	
+    }
+    public void addMoreThanOneNumbersTest() {
+    	assertEquals(44, strCalc.Add("12,24,8"));
     	
     }
 }
